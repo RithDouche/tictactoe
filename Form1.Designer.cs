b6149df1776902ebfe2202abfa54a2f8b9dbcdec
Rith <siengsotharith@gmail.com>
2025-02-11 14:34:35 -0800

    stash to pull

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -67,26 +67,26 @@
             this.tableLayoutPanel1.Controls.Add(this.userSC, 1, 0);
             this.tableLayoutPanel1.Controls.Add(this.computerSC, 2, 0);
             this.tableLayoutPanel1.Controls.Add(this.winPercentage, 3, 0);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(99, 47);
-            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.tableLayoutPanel1.MinimumSize = new System.Drawing.Size(836, 465);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(136, 70);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.tableLayoutPanel1.MinimumSize = new System.Drawing.Size(1150, 698);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 4;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(836, 465);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1150, 698);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // button2
             // 
             this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(3, 118);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Location = new System.Drawing.Point(4, 177);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(203, 112);
+            this.button2.Size = new System.Drawing.Size(279, 168);
             this.button2.TabIndex = 32;
             this.button2.Text = "   New Game   (Counts as a loss)\r\n";
             this.button2.UseVisualStyleBackColor = true;
@@ -95,100 +95,100 @@
             // 
             this.turn.Dock = System.Windows.Forms.DockStyle.Fill;
             this.turn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.turn.Location = new System.Drawing.Point(3, 2);
-            this.turn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.turn.Location = new System.Drawing.Point(4, 3);
+            this.turn.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.turn.Name = "turn";
-            this.turn.Size = new System.Drawing.Size(203, 24);
+            this.turn.Size = new System.Drawing.Size(279, 31);
             this.turn.TabIndex = 31;
             this.turn.Text = "Turn: ";
             // 
             // bottomL
             // 
             this.bottomL.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.bottomL.Location = new System.Drawing.Point(212, 350);
-            this.bottomL.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.bottomL.Location = new System.Drawing.Point(291, 525);
+            this.bottomL.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.bottomL.Name = "bottomL";
-            this.bottomL.Size = new System.Drawing.Size(203, 113);
+            this.bottomL.Size = new System.Drawing.Size(279, 170);
             this.bottomL.TabIndex = 26;
             this.bottomL.UseVisualStyleBackColor = true;
             // 
             // bottomM
             // 
             this.bottomM.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.bottomM.Location = new System.Drawing.Point(421, 350);
-            this.bottomM.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.bottomM.Location = new System.Drawing.Point(578, 525);
+            this.bottomM.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.bottomM.Name = "bottomM";
-            this.bottomM.Size = new System.Drawing.Size(203, 113);
+            this.bottomM.Size = new System.Drawing.Size(279, 170);
             this.bottomM.TabIndex = 25;
             this.bottomM.UseVisualStyleBackColor = true;
             // 
             // bottomR
             // 
             this.bottomR.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.bottomR.Location = new System.Drawing.Point(630, 350);
-            this.bottomR.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.bottomR.Location = new System.Drawing.Point(865, 525);
+            this.bottomR.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.bottomR.Name = "bottomR";
-            this.bottomR.Size = new System.Drawing.Size(203, 113);
+            this.bottomR.Size = new System.Drawing.Size(281, 170);
             this.bottomR.TabIndex = 24;
             this.bottomR.UseVisualStyleBackColor = true;
             // 
             // middleL
             // 
             this.middleL.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.middleL.Location = new System.Drawing.Point(212, 234);
-            this.middleL.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.middleL.Location = new System.Drawing.Point(291, 351);
+            this.middleL.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.middleL.Name = "middleL";
-            this.middleL.Size = new System.Drawing.Size(203, 112);
+            this.middleL.Size = new System.Drawing.Size(279, 168);
             this.middleL.TabIndex = 22;
             this.middleL.UseVisualStyleBackColor = true;
             // 
             // middleM
             // 
             this.middleM.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.middleM.Location = new System.Drawing.Point(421, 234);
-            this.middleM.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.middleM.Location = new System.Drawing.Point(578, 351);
+            this.middleM.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.middleM.Name = "middleM";
-            this.middleM.Size = new System.Drawing.Size(203, 112);
+            this.middleM.Size = new System.Drawing.Size(279, 168);
             this.middleM.TabIndex = 21;
             this.middleM.UseVisualStyleBackColor = true;
             // 
             // middleR
             // 
             this.middleR.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.middleR.Location = new System.Drawing.Point(630, 234);
-            this.middleR.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.middleR.Location = new System.Drawing.Point(865, 351);
+            this.middleR.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.middleR.Name = "middleR";
-            this.middleR.Size = new System.Drawing.Size(203, 112);
+            this.middleR.Size = new System.Drawing.Size(281, 168);
             this.middleR.TabIndex = 20;
             this.middleR.UseVisualStyleBackColor = true;
             // 
             // topL
             // 
             this.topL.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.topL.Location = new System.Drawing.Point(212, 118);
-            this.topL.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.topL.Location = new System.Drawing.Point(291, 177);
+            this.topL.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.topL.Name = "topL";
-            this.topL.Size = new System.Drawing.Size(203, 112);
+            this.topL.Size = new System.Drawing.Size(279, 168);
             this.topL.TabIndex = 18;
             this.topL.UseVisualStyleBackColor = true;
             // 
             // topM
             // 
             this.topM.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.topM.Location = new System.Drawing.Point(421, 118);
-            this.topM.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.topM.Location = new System.Drawing.Point(578, 177);
+            this.topM.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.topM.Name = "topM";
-            this.topM.Size = new System.Drawing.Size(203, 112);
+            this.topM.Size = new System.Drawing.Size(279, 168);
             this.topM.TabIndex = 17;
             this.topM.UseVisualStyleBackColor = true;
             // 
             // topR
             // 
             this.topR.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.topR.Location = new System.Drawing.Point(630, 118);
-            this.topR.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.topR.Location = new System.Drawing.Point(865, 177);
+            this.topR.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.topR.Name = "topR";
-            this.topR.Size = new System.Drawing.Size(203, 112);
+            this.topR.Size = new System.Drawing.Size(281, 168);
             this.topR.TabIndex = 16;
             this.topR.UseVisualStyleBackColor = true;
             // 
@@ -196,10 +196,10 @@
             // 
             this.userSC.Dock = System.Windows.Forms.DockStyle.Fill;
             this.userSC.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.userSC.Location = new System.Drawing.Point(212, 2);
-            this.userSC.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.userSC.Location = new System.Drawing.Point(291, 3);
+            this.userSC.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.userSC.Name = "userSC";
-            this.userSC.Size = new System.Drawing.Size(203, 24);
+            this.userSC.Size = new System.Drawing.Size(279, 31);
             this.userSC.TabIndex = 14;
             this.userSC.Text = "User score -";
             // 
@@ -207,10 +207,10 @@
             // 
             this.computerSC.Dock = System.Windows.Forms.DockStyle.Fill;
             this.computerSC.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.computerSC.Location = new System.Drawing.Point(423, 5);
-            this.computerSC.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.computerSC.Location = new System.Drawing.Point(581, 8);
+            this.computerSC.Margin = new System.Windows.Forms.Padding(7, 8, 7, 8);
             this.computerSC.Name = "computerSC";
-            this.computerSC.Size = new System.Drawing.Size(199, 24);
+            this.computerSC.Size = new System.Drawing.Size(273, 31);
             this.computerSC.TabIndex = 13;
             this.computerSC.Text = "Computer Score -";
             // 
@@ -218,21 +218,21 @@
             // 
             this.winPercentage.Dock = System.Windows.Forms.DockStyle.Fill;
             this.winPercentage.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.winPercentage.Location = new System.Drawing.Point(632, 5);
-            this.winPercentage.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.winPercentage.Location = new System.Drawing.Point(868, 8);
+            this.winPercentage.Margin = new System.Windows.Forms.Padding(7, 8, 7, 8);
             this.winPercentage.Name = "winPercentage";
-            this.winPercentage.Size = new System.Drawing.Size(199, 24);
+            this.winPercentage.Size = new System.Drawing.Size(275, 31);
             this.winPercentage.TabIndex = 12;
             this.winPercentage.Text = "Win % -";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1055, 663);
+            this.ClientSize = new System.Drawing.Size(1451, 994);
             this.Controls.Add(this.tableLayoutPanel1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.MaximumSize = new System.Drawing.Size(2522, 1214);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximumSize = new System.Drawing.Size(3459, 1789);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
